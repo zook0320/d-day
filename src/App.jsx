f896ea4cc6dd74d012f481282bc02be619ad5434
윤준수 <99115509+hoheesu@users.noreply.github.com>
2024-02-13 23:46:50 +0900

    Feat: develop 병합 전 커밋

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Modal from "./Modal";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Modal />
+    </div>
+  );
 }
 
 export default App;
--- src/Modal.jsx
+++ src/Modal.jsx
@@ -4,18 +4,18 @@ function Modal() {
   return (
     <div>
       <form>
-        <input type="text" />
-        <select name="languages" id="lang">
-          <option value="javascript">JavaScript</option>
-          <option value="php">PHP</option>
-          <option value="java">Java</option>
-          <option value="golang">Golang</option>
-          <option value="python">Python</option>
-          <option value="c#">C#</option>
-          <option value="C++">C++</option>
-          <option value="erlang">Erlang</option>
-        </select>
-        <input type="date" />
+        <p>
+          <input type="text" placeholder="기념일 입력" />
+        </p>
+        <p>
+          <select name="" id="lang">
+            <option value="javascript">JavaScript</option>
+            <option value="php">PHP</option>
+          </select>
+        </p>
+        <p>
+          <input type="date" />
+        </p>
       </form>
     </div>
   );
